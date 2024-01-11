d6a076120d9271e1ac44dc4773e5c1a0cb27f377
rikzafauzannf <rikzarazor491@gmail.com>
2024-01-11 17:29:02 +0700

    up v2 complete

--- main.js
+++ main.js
@@ -71,3 +71,26 @@ if (stackCards.length > 0 && intersectionObserverSupported) {
     new StackCards(stackCards[i]);
   }
 }
+
+// playbutton
+$(document).ready(function () {
+  // get video source from data-src button
+  var $videoSrc;
+  $(".video-btn").click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
+  // autoplay video on modal load
+  $("#myModal").on("shown.bs.modal", function (e) {
+    // youtube iframe configuration and settings
+    $("#video").attr(
+      "src",
+      $videoSrc + "?autoplay=1&rel=0&controls=1&loop=1&modestbranding=1"
+    );
+  });
+  // stop playing the youtube video when modal closed
+  $("#myModal").on("hide.bs.modal", function (e) {
+    // stop video
+    $("#video").attr("src", $videoSrc);
+  });
+});
